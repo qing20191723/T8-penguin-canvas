ce0cfd08ebc5f64a1841c9472f85726ce70451af
qing20191723 <131326843+qing20191723@users.noreply.github.com>
2026-08-01 23:53:26 +0800

    style: load Atlas-only runtime presentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { lazy, StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+import './styles/atlasOnly.css';
 import { installPublicCollaborationPolicyTransport } from './utils/publicCollaborationPolicyTransport';
 
 installPublicCollaborationPolicyTransport();
